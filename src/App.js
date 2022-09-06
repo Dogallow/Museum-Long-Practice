80a4c06d4d3c6ad4edafdafc6ffd0f9be529dd57
Bpost129 <Bpost129@yahoo.com>
2022-09-06 19:55:36 -0400

    phase 2 kinda

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+import { React } from 'react'
 import harvardArt from "./data/harvardArt";
+import GalleryNavigation from "./components/GalleryNavigation";
 
 function App() {
   
   return (
+    <>
     <h1>Hello from App</h1>
+    <GalleryNavigation galleries = { harvardArt.records }/>
+    </>
   );
 }
 
---
+++ src/components/GalleryNavigation/index.js
@@ -0,0 +1,9 @@
+
+
+const GalleryNavigation = ({ galleries }) => {
+    return (
+        <h1>{ galleries.records }</h1>
+    )
+}
+
+export default GalleryNavigation;
